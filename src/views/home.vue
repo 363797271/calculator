de0cc3afe074347625371ecf0d614b062fed1be9
zyd <363797271@qq.com>
2020-11-21 16:29:46 +0800

    nav

--- src/views/home.vue
+++ src/views/home.vue
@@ -3,9 +3,9 @@
     <van-nav-bar title="首页" />
     <van-grid>
       <van-grid-item icon="photo-o" text="分期缴费计算器" @click="toPage('installment')" />
+      <!-- <van-grid-item icon="photo-o" text="文字" />
       <van-grid-item icon="photo-o" text="文字" />
-      <van-grid-item icon="photo-o" text="文字" />
-      <van-grid-item icon="photo-o" text="文字" />
+      <van-grid-item icon="photo-o" text="文字" /> -->
     </van-grid>
   </div>
 </template>
