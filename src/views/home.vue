9170247a345432f4fc30a10c10f67ac3e2640bc8
张英丹 <15683901122@139.com>
2020-11-30 09:32:57 +0800

    1

--- src/views/home.vue
+++ src/views/home.vue
@@ -8,11 +8,6 @@
       <van-grid-item icon="photo-o" text="文字" />
       <van-grid-item icon="photo-o" text="文字" /> -->
     </van-grid>
-    <div class="info">
-      <h3>一些说明：</h3>
-      <p>新生儿年龄是0岁开始；</p>
-      <p>每一期展示的都是这一期结束产生的费用，例如第一期（年）缴费1000，产生的利息就是1000一年的利息，总花费就是1000+利息</p>
-    </div>
   </div>
 </template>
 
